a639c24a7804e15815b8ab78a772d295243cba7c
ShubhamVerma1994 <palluverma1994@gmail.com>
2019-08-25 19:18:38 +0530

    deleted file

--- to_check_count_string.py
+++
@@ -1,9 +0,0 @@
-str_1 = str(input("Enter a string = "))
-dict_str = {}
-for i in str_1:
-    keys = dict_str.keys()
-    if i in keys:
-        dict_str[i] += 1
-    else:
-        dict_str[i] = 1
-print(dict_str)
